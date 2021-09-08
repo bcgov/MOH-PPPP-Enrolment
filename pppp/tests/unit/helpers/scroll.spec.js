07a24406044a265c51abee7c721c916e589cd3de
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-08 11:29:22 -0700

    collect jest test coverage, add scroll test

---
+++ pppp/tests/unit/helpers/scroll.spec.js
@@ -0,0 +1,17 @@
+import * as scroll from "@/helpers/scroll.js";
+
+const spyOnWindowScrollTo = jest
+  .spyOn(window, "scrollTo")
+  .mockImplementation(jest.fn);
+
+describe("Helper scroll.js scrollTo()", () => {
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("calls window.scrollTo() when helper scrollTo() is called", () => {
+    scroll.scrollTo(0);
+    expect(spyOnWindowScrollTo).toHaveBeenCalled();
+  });
+});
