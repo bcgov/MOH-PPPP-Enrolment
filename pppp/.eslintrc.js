31fc146fcc9c38d7ba7c007049583f278975ed01
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-08 12:32:30 -0700

    add eslint-plugin-jest and eslint-plugin-jest-formatting to aid with development, change .eslintrc.js settings

--- pppp/.eslintrc.js
+++ pppp/.eslintrc.js
@@ -5,15 +5,28 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:jest-formatting/recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
+  plugins: [
+    "jest", 
+    "jest-formatting"
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'indent': ["error", 2]
+    'indent': ["error", 2],
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "warn",
+    "jest/no-identical-title": "warn",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
+    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
   },
   overrides: [
     {
