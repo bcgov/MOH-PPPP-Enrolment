fd8408085d64b032b878cded6447087602e6d584
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-08 14:49:58 -0700

    validators.spec.js clinicNameValidator() tests

--- pppp/tests/unit/helpers/validators.spec.js
+++ pppp/tests/unit/helpers/validators.spec.js
@@ -30,3 +30,34 @@ describe("validators.js bcPostalCodeValidator()", () => {
     expect(result).toEqual(false);
   });
 });
+
+describe("validators.js clinicNameValidator()", () => {
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  it("returns true if given an empty value", () => {
+    const result = clinicNameValidator();
+    expect(result).toEqual(true);
+  });
+
+  it("returns true if given a string containing letters and numbers", () => {
+    const result = clinicNameValidator("AAAaaa111");
+    expect(result).toEqual(true);
+  });
+
+  it("returns true if given a string containing hyphens, periods, apostrophes, and spaces", () => {
+    const result = clinicNameValidator("-.' -.' -.' ");
+    expect(result).toEqual(true);
+  });
+
+  it("returns false if given a string containing special characters", () => {
+    const result = clinicNameValidator("ÁḚȈỐÚ");
+    expect(result).toEqual(false);
+  });
+
+  it("returns false if given a string containing other punctuation", () => {
+    const result = clinicNameValidator("()@#$%^&*");
+    expect(result).toEqual(false);
+  });
+});
