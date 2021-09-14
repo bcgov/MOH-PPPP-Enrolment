ea9bf00a73a45fa4bd6d3fbce06c71e8d16e2d08
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-14 14:18:51 -0700

    computed value tests planReferenceNumberData()

--- pppp/tests/unit/components/ReviewTableListPatient.spec.js
+++ pppp/tests/unit/components/ReviewTableListPatient.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, createLocalVue } from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import Vue from "vue";
 import Vuelidate from "vuelidate";
@@ -13,22 +13,23 @@ localVue.use(Vuex);
 Vue.use(Vuelidate);
 
 const storeTemplate = {
-    modules: {
-      app: cloneDeep(module1),
-      payPatientForm: cloneDeep(module2),
-      payPractitionerForm: cloneDeep(module3),
-    },
-  };
+  modules: {
+    app: cloneDeep(module1),
+    payPatientForm: cloneDeep(module2),
+    payPractitionerForm: cloneDeep(module3),
+  },
+};
 
 const patientState = {
-    medicalServiceClaims: ["1", "2", "3"]
-}
-storeTemplate.modules.payPatientForm.state = cloneDeep(patientState)
+  medicalServiceClaims: ["1", "2", "3"],
+  planReferenceNumber: "11111",
+};
+storeTemplate.modules.payPatientForm.state = cloneDeep(patientState);
 
-describe("ReviewTableList --- patient.vue", () => {
+describe.skip("ReviewTableList -- patient", () => {
   it("renders", () => {
     const store = new Vuex.Store(storeTemplate);
-    const wrapper = shallowMount(Page, {
+    const wrapper = mount(Page, {
       localVue,
       store,
       mocks: {
@@ -44,5 +45,55 @@ describe("ReviewTableList --- patient.vue", () => {
       },
     });
     expect(wrapper.element).toBeDefined();
+  });
+});
+
+describe("ReviewTableList -- patient planReferenceNumberData() CSR", () => {
+  let store;
+  let wrapper;
+
+  it("returns plan reference number in store when path isCSR", () => {
+    //please note the route change between this and the next test
+    store = new Vuex.Store(storeTemplate);
+    wrapper = mount(Page, {
+      localVue,
+      store,
+      mocks: {
+        $route: {
+          path: "/",
+        },
+        $router: {
+          push: jest.fn(),
+          currentRoute: {
+            path: "/potato-csr",
+          },
+        },
+      },
+    });
+    const result = wrapper.vm.$store.state.payPatientForm.planReferenceNumber;
+    expect(result).toBe("11111");
+    expect(wrapper.text()).toEqual(expect.stringContaining("11111"));
+  });
+
+  it("does not render plan reference number when path is NOT CSR", () => {
+    store = new Vuex.Store(storeTemplate);
+    wrapper = mount(Page, {
+      localVue,
+      store,
+      mocks: {
+        $route: {
+          path: "/",
+        },
+        $router: {
+          push: jest.fn(),
+          currentRoute: {
+            path: "/potato",
+          },
+        },
+      },
+    });
+    const result = wrapper.vm.$store.state.payPatientForm.planReferenceNumber;
+    expect(result).toBe("11111");
+    expect(wrapper.text()).not.toEqual(expect.stringContaining("11111"));
   });
 });
