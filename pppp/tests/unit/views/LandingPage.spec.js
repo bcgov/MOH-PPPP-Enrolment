ae248894ade47cd700d4a4dd3eee13a1057992b4
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-21 10:31:12 -0700

    LandingPage.vue render test

---
+++ pppp/tests/unit/views/LandingPage.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
+import Vue from "vue";
+import Vuelidate from "vuelidate";
+import Page from "@/views/LandingPage.vue";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+Vue.use(Vuelidate);
+
+describe("LandingPage.vue", () => {
+  it("renders", () => {
+    const wrapper = shallowMount(Page, {
+      localVue,
+    });
+    expect(wrapper.element).toBeDefined();
+  });
+});
