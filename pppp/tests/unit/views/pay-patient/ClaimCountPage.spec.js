d35c36377925b44ae7596c0f0941adddb9da0825
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-22 11:18:29 -0700

    comments on created(), isFirstLoad() test

--- pppp/tests/unit/views/pay-patient/ClaimCountPage.spec.js
+++ pppp/tests/unit/views/pay-patient/ClaimCountPage.spec.js
@@ -7,10 +7,12 @@ import { cloneDeep } from "lodash";
 import * as module1 from "../../../../src/store/modules/app";
 import * as module2 from "../../../../src/store/modules/pay-patient-form";
 import * as module3 from "../../../../src/store/modules/pay-practitioner-form";
+import logService from "@/services/log-service";
+import spaEnvService from "@/services/spa-env-service";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
-Vue.use(Vuelidate);
+localVue.use(Vuelidate);
 
 const storeTemplate = {
   modules: {
@@ -22,15 +24,21 @@ const storeTemplate = {
 
 const patientState = {
   isInfoCollectionNoticeOpen: true,
+  applicationUuid: null,
 };
 
 storeTemplate.modules.payPatientForm.state = cloneDeep(patientState);
+
+const spyOnLogService = jest
+  .spyOn(logService, "logNavigation")
+  .mockImplementation(() => Promise.resolve("logged"));
 
 console.log(
   "******************************************************************************************************************************************"
 );
 
-describe("ClaimCountPage.vue", () => {
+describe("ClaimCountPage.vue render test", () => {
+  console.log("render test: *****************");
   let store;
   let wrapper;
 
@@ -53,7 +61,109 @@ describe("ClaimCountPage.vue", () => {
     });
   });
 
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
   it("renders", () => {
     expect(wrapper.element).toBeDefined();
   });
+});
+
+describe("ClaimCountPage.vue created()", () => {
+  let store;
+  let wrapper;
+
+  beforeEach(() => {
+    store = new Vuex.Store(storeTemplate);
+    wrapper = mount(Page, {
+      localVue,
+      store,
+      mocks: {
+        $route: {
+          path: "/",
+        },
+        $router: {
+          push: jest.fn(),
+          currentRoute: {
+            path: "/potato-csr",
+          },
+        },
+      },
+    });
+
+    wrapper.vm.$options.created.forEach((hook) => {
+      hook.call(wrapper.vm);
+    });
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("calls logService", () => {
+    expect(spyOnLogService).toHaveBeenCalled();
+  });
+
+  /* 
+  I spent a number of hours trying to test the code in the if (this.isFirstLoad()) {} block. 
+  I came to the conclusion that it may not be possible to do for lifecycle reasons.
+  The order of operations looks like this:
+  1. Created() test runs
+  2. New Store is created
+  3. The if (this.isFirstLoad()) {} code block recognizes that there isn't a UUID, so it assigns one
+  4. The if block calls all of the other functions inside the if block
+  5. The mounted() method creates any spies to check to see if those functions are called
+  6. If you call the created() hook a second time, the if block sees there's already a UUID
+  So it doesn't call anything in the if block a second time
+  7. The spies don't catch the function calls because they're created after the function is called
+  And it's not really possible to call them a second time without refactoring the code
+
+  If it's essential that these functions be tested, the contents of the if block would need to be moved to a separate function
+  Then that function can be called independently of the if block and tested that way
+  Until then, I leave these comments as an aid to anyone who tries to attempt this in the future
+  */
+});
+
+describe("ClaimCountPage.vue isFirstLoad()", () => {
+  let store;
+  let wrapper;
+
+  beforeEach(() => {
+    store = new Vuex.Store(storeTemplate);
+    wrapper = mount(Page, {
+      localVue,
+      store,
+      mocks: {
+        $route: {
+          path: "/",
+        },
+        $router: {
+          push: jest.fn(),
+          currentRoute: {
+            path: "/potato-csr",
+          },
+        },
+      },
+    });
+
+    wrapper.vm.$options.created.forEach((hook) => {
+      hook.call(wrapper.vm);
+    });
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("returns false", () => {
+    const result = wrapper.vm.isFirstLoad()
+    expect(result).toEqual(false);
+  });
+
+  //the true version of this test is impossible to verify
+  //for the reasons stated in the created() section
 });
