c24e7397bcca75d35227b5f7cf34562872191370
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-24 11:14:56 -0700

    HomePage.spec patient created() dispatch test

--- pppp/tests/unit/views/pay-patient/HomePage.spec.js
+++ pppp/tests/unit/views/pay-patient/HomePage.spec.js
@@ -1,21 +1,37 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import Vuex from 'vuex';
-import Vue from 'vue';
-import Vuelidate from 'vuelidate';
-import YourInfoPage from '@/views/pay-patient/HomePage.vue';
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
+import Vue from "vue";
+import Vuelidate from "vuelidate";
+import { cloneDeep } from "lodash";
+import Page from "@/views/pay-patient/HomePage.vue";
+import * as module1 from "../../../../src/store/modules/app";
+import * as module2 from "../../../../src/store/modules/pay-patient-form";
+import * as module3 from "../../../../src/store/modules/pay-practitioner-form";
+import * as dummyDataValid from "../../../../src/store/states/pay-patient-form-dummy-data";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 Vue.use(Vuelidate);
 
-describe('Pay Patient - HomePage.vue', () => {
+const storeTemplate = {
+  modules: {
+    app: cloneDeep(module1.default),
+    payPatientForm: cloneDeep(module2.default),
+    payPractitionerForm: cloneDeep(module3.default),
+  },
+};
+
+const patientState = cloneDeep(dummyDataValid.default);
+storeTemplate.modules.payPatientForm.state = cloneDeep(patientState);
+
+describe("HomePage.vue pay patient", () => {
   let state;
   let store;
 
   beforeEach(() => {
-    state ={
+    state = {
       applicationUuid: null,
-    };    
+    };
 
     store = new Vuex.Store({
       modules: {
@@ -23,17 +39,68 @@ describe('Pay Patient - HomePage.vue', () => {
           state,
           namespaced: true,
           actions: {
-            setApplicationUuid() {}
-          }
-        }
-      }
+            setApplicationUuid() {},
+          },
+        },
+      },
     });
   });
-  it('renders', () => {
-    const wrapper = shallowMount(YourInfoPage, {
+
+  it("renders", () => {
+    const wrapper = shallowMount(Page, {
       store,
       localVue,
     });
     expect(wrapper.element).toBeDefined();
+  });
+});
+
+describe("HomePage.vue pay patient created()", () => {
+  let store;
+  let wrapper;
+  let $route;
+  let $router;
+  let spyOnRouter;
+  let spyOnDispatch;
+
+  beforeEach(() => {
+    store = new Vuex.Store(storeTemplate);
+    $route = {
+      path: "/potato-csr",
+    };
+    $router = {
+      $route,
+      currentRoute: $route,
+      push: jest.fn(),
+    };
+    wrapper = mount(Page, {
+      localVue,
+      store,
+      mocks: {
+        $route,
+        $router,
+      },
+    });
+    spyOnDispatch = jest.spyOn(wrapper.vm.$store, "dispatch");
+
+    spyOnRouter = jest
+      .spyOn($router, "push")
+      .mockImplementation(() => Promise.resolve("pushed"));
+
+    wrapper.vm.$options.created.forEach((hook) => {
+      hook.call(wrapper.vm);
+    });
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("calls dispatch with correct parameters", () => {
+    expect(spyOnDispatch).toHaveBeenCalledWith(
+      `${module2.MODULE_NAME}/${module2.SET_APPLICATION_UUID}`,
+      wrapper.vm.applicationUuid
+    );
   });
 });
