6a6d9a9bca49312dd7aa55ff8ccf32b25b3fd784
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-09-28 16:54:09 -0700

    update test exclusion list

--- pppp/jest.config.js
+++ pppp/jest.config.js
@@ -4,8 +4,12 @@ module.exports = {
   collectCoverageFrom: [
     "src/**/*.{js,vue}",
     "!src/main.js", // No need to cover bootstrap file
-    "!src/constants/*", // Nothing to test here either
+    "!src/constants/*", // Entirely constant exports
+    "!src/settings.js", // Entirely constant exports
     "!src/store/states/*", // Entirely constant exports
+    "!src/store/index.js", // Entirely constant exports
+    "!src/locale/strings.en.js", // Entirely constant exports
     "!src/helpers/form-helpers.js", // Entirely constant exports
+    "!src/helpers/user-agent.js", // Not really reasonable/possible to test in unit tests
   ]
 }
