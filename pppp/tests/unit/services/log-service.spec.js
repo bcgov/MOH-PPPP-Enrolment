be9abb4de02a372299c0b6f87235ff9b208e3d46
Stephanie <stephanie.harrison@maximuscanada.ca>
2021-10-04 13:47:02 -0700

    logService _sendLog test

--- pppp/tests/unit/services/log-service.spec.js
+++ pppp/tests/unit/services/log-service.spec.js
@@ -9,17 +9,49 @@ jest.mock("axios", () => ({
   }),
 }));
 
+const spyOnAxiosPost = jest.spyOn(axios, "post")
+
+const spyOnSendLog = jest.spyOn(logService, "_sendLog")
+
 const mockResponse = {};
 
 axios.post.mockImplementation(() => Promise.resolve(mockResponse));
 
-describe("LogService test", () => {
+describe("LogService", () => {
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("logSubmission calls _sendLog()", () => {
+    logService.logSubmission("1", "2", "3")
+    expect(spyOnSendLog).toHaveBeenCalledWith("info", "1", "2", "3");
+  });
+
+  it("logError calls _sendLog()", () => {
+    logService.logError("1", "2")
+    expect(spyOnSendLog).toHaveBeenCalledWith("error", "1", "2");
+  });
+
+  it("logInfo calls _sendLog()", () => {
+    logService.logInfo("1", "2")
+    expect(spyOnSendLog).toHaveBeenCalledWith("info", "1", "2",);
+  });
+
+  it("logNavigation calls _sendLog()", () => {
+    logService.logNavigation("1", "2", "3")
+    expect(spyOnSendLog).toHaveBeenCalled();
+  });
+});
+
+describe("LogService _sendLog", () => {
     afterEach(() => {
       jest.resetModules();
       jest.clearAllMocks();
     });
   
-    it("renders", () => {
-      expect(logService).toBeDefined();
+    it("calls axios", () => {
+      logService.logSubmission("1", "2", "3")
+      expect(spyOnAxiosPost).toHaveBeenCalled();
     });
   });
