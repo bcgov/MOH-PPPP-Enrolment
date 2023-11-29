3455658b9d0ea1be3b7c21f58bc037ed213ee019
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2023-11-29 15:29:46 -0800

    unit test vue3 update TextArea TipBox

--- pppp/tests/unit/components/TextArea.spec.js
+++ pppp/tests/unit/components/TextArea.spec.js
@@ -1,25 +1,14 @@
-import { mount, createLocalVue } from "@vue/test-utils";
-import Vuex from "vuex";
-import Vue from "vue";
-import Vuelidate from "vuelidate";
+import { mount } from "@vue/test-utils";
 import Component from "@/components/TextArea.vue";
-
-const localVue = createLocalVue();
-localVue.use(Vuex);
-Vue.use(Vuelidate);
 
 describe("TextArea.vue", () => {
   it("renders", () => {
-    const wrapper = mount(Component, {
-      localVue,
-    });
+    const wrapper = mount(Component);
     expect(wrapper.element).toBeDefined();
   });
 
   it("emits input correctly through built in method", () => {
-    const wrapper = mount(Component, {
-      localVue,
-    });
+    const wrapper = mount(Component);
     const fakeEvent = { target: { value: "potato" } };
     wrapper.vm.emitInput(fakeEvent);
     expect(wrapper.emitted().input).toBeTruthy();
