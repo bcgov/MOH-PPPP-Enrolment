b8e1dbcf89485dafe5750a8c0f8d981ff23fa44c
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2023-12-06 09:16:27 -0800

    add emit test for handleInputServiceFeeItem

--- pppp/tests/unit/components/MedicalServiceClaimsFormItem.spec.js
+++ pppp/tests/unit/components/MedicalServiceClaimsFormItem.spec.js
@@ -1,11 +1,6 @@
 import { mount } from "@vue/test-utils";
-import { createStore } from "vuex";
 import { cloneDeep } from "lodash";
-import * as module1 from "../../../src/store/modules/app";
-import * as module2 from "../../../src/store/modules/pay-patient-form";
-import * as module3 from "../../../src/store/modules/pay-practitioner-form";
 import * as dummyDataPatient from "../../../src/store/states/pay-patient-form-dummy-data";
-import * as dummyDataPractitioner from "../../../src/store/states/pay-practitioner-form-dummy-data";
 //in the long term, I want to move this into the components folder
 //but for now I'll import it from its current location in /views
 import Component from "@/views/pay-patient/MedicalServiceClaimsFormItem";
@@ -25,39 +20,68 @@ const mockRouter = {
   },
 };
 
-const storeTemplate = {
-  modules: {
-    app: cloneDeep(module1.default),
-    payPatientForm: cloneDeep(module2.default),
-    payPractitionerForm: cloneDeep(module3.default),
-  },
-};
+const passingClaim = cloneDeep(
+  dummyDataPatient.default.medicalServiceClaims[0]
+);
 
-const patientState = cloneDeep(dummyDataPatient.default);
-const practitionerState = cloneDeep(dummyDataPractitioner.default);
-storeTemplate.modules.payPatientForm.state = cloneDeep(patientState);
-storeTemplate.modules.payPractitionerForm.state = cloneDeep(practitionerState);
+const passingProps = {
+  index: 0,
+  serviceDateData: passingClaim.serviceDateData,
+  numberOfServices: passingClaim.numberOfServices,
+  serviceDate: passingClaim.serviceDate,
+  feeItem: passingClaim.feeItem,
+  amountBilled: passingClaim.amountBilled,
+  calledStartTime: passingClaim.calledStartTime,
+  renderedFinishTime: passingClaim.renderedFinishTime,
+  diagnosticCode: passingClaim.diagnosticCode,
+  locationOfService: passingClaim.locationOfService,
+  serviceClarificationCode: passingClaim.serviceClarificationCode,
+  correspondenceAttached: passingClaim.correspondenceAttached,
+  submissionCode: passingClaim.submissionCode,
+  notes: passingClaim.notes,
+  medicalServiceClaimsFeeItemValidationError: false,
+}
 
 describe("MedicalServiceClaimsFormItem.vue", () => {
-  // eslint-disable-next-line
-  let state;
-  let store;
   let wrapper;
 
   beforeEach(() => {
-    state = {
-      applicationUuid: null,
-    };
-    store = createStore(storeTemplate);
     wrapper = mount(Component, {
       global: {
-        plugins: [store],
         mocks: mockRouter,
+        props: passingProps,
       },
     });
   });
 
   it("renders", () => {
     expect(wrapper.element).toBeDefined();
+  });
+});
+
+describe("MedicalServiceClaimsFormItem handleInputServiceFeeItem()", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(Component, {
+      global: {
+        mocks: mockRouter,
+        props: passingProps,
+      },
+    });
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
+
+  it("emits update:feeItem and update:medicalServiceClaimsFeeItemValidationError", async () => {
+    expect(wrapper.emitted()).not.toHaveProperty('update:feeItem')
+    expect(wrapper.emitted()).not.toHaveProperty('update:medicalServiceClaimsFeeItemValidationError')
+    wrapper.vm.handleInputServiceFeeItem(0);
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted()).toHaveProperty('update:feeItem')
+    expect(wrapper.emitted()).toHaveProperty('update:medicalServiceClaimsFeeItemValidationError')
   });
 });
