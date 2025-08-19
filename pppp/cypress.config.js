40e0354bef5921d6643ccf44666a5865c298c40b
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2025-08-19 10:46:20 -0700

    upgrade cypress to 14.0.0+

--- pppp/cypress.config.js
+++ pppp/cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://172.25.129.209:5173/ahdc/",
     experimentalRunAllSpecs: true,
-    testIsolation: "on", //to prevent Cypress errors
+    testIsolation: true, //to prevent Cypress errors
     experimentalSessionAndOrigin: true,
     // setupNodeEvents(on) {
     //   on('file:preprocessor', vitePreprocessor())
