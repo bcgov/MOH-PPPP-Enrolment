2dafb21b3105c5771600f67cebd2d53e3536bce9
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2025-08-19 12:22:10 -0700

    update start-server-and-test to 2.0.13

--- pppp/cypress.config.js
+++ pppp/cypress.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     baseUrl: "http://172.25.129.209:5173/ahdc/",
     experimentalRunAllSpecs: true,
     testIsolation: true, //to prevent Cypress errors
-    experimentalSessionAndOrigin: true,
     // setupNodeEvents(on) {
     //   on('file:preprocessor', vitePreprocessor())
     // },
